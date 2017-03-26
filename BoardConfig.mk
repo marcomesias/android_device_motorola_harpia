df6412621d63b8f16ebe094f9fe81b678510bf45
Subhrajyoti Sen <subhrajyoti12@gmail.com>
2017-03-26 11:26:15 +0530

    Use GCC toolchain

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,6 +31,7 @@ TARGET_USE_SDCLANG := true
 
 # Kernel
 TARGET_KERNEL_CONFIG := harpia_defconfig
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := $(PWD)/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi- 
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216     # 16384 * 1024 mmcblk0p31
