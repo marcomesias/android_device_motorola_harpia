2144ff670fde733ae390c830d38ce57c5c42c34a
dark98 <darkaidz98@gmail.com>
2017-06-13 01:24:56 +0100

    Initial Try To Carbonify Device Tree

--- carbon_harpia.mk
+++ carbon_harpia.mk
@@ -15,13 +15,21 @@
 
 $(call inherit-product, device/motorola/harpia/full_harpia.mk)
 
+# Inherit Carbon GSM telephony parts
+
+$(call inherit-product, vendor/carbon/config/gsm.mk)
+
+# Inherit Carbon product configuration
+
+$(call inherit-product, vendor/carbon/config/common.mk)
+
 # Boot animation
 TARGET_SCREEN_WIDTH := 720
 TARGET_SCREEN_HEIGHT := 1280
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := harpia
-PRODUCT_NAME := lineage_harpia
+PRODUCT_NAME := carbon_harpia
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_RELEASE_NAME := harpia
